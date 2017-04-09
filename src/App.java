5fc318d586b971f3f39527467c6decf62a697bc7
oscarlondono <oscarlondono@simplesolutions.com.co>
2017-04-09 11:03:49 -0500

    Methods
    Describe how to write some class methods

--- src/App.java
+++ src/App.java
@@ -8,6 +8,17 @@ class Person {
 	   1. Data 
 	   2. Subroutines (methods)
 	 */
+	void speak() {
+		System.out.println("My name is " + name + " and I am " + age + " years old." );
+	}
+	
+	void sayHello() {
+		System.out.println("Hello there!");
+	}
+	
+	void sayMyName(){
+		System.out.println("My name is " + name + "." );
+	}
 }
 public class App {
 
@@ -15,11 +26,15 @@ public class App {
 		Person person1 = new Person();
 		person1.name = "Newman";
 		person1.age = 42;
-
+		person1.speak();
+		person1.sayMyName();
+		
 		Person person2 = new Person();
 		person2.name = "Kramer";
 		person2.age = 38;
-		
+		person2.speak();
+		person2.sayHello();
+		System.out.println(person2.name);
 	}
 
 }
